ae59443e4671c402065ba26e1d18feacbfa76b51
soom93 <soom93@gmail.com>
2019-06-02 16:25:28 +0900

    T2. 화폐 단위 Money class 적용

--- money.rb
+++ money.rb
@@ -1,11 +1,14 @@
+require './money_type'
 class Money
+  include MONEY_TYPE
+
   attr_accessor :value
-  MONEY_TYPE = [:_50, :_100, :_500, :_1000, :_5000]
+ 
   def initialize(money)
     money_valid("_"+money.to_s) ? @value = money : puts("잘못 된 화폐입니다!")
   end
 
   def money_valid(value)
-    MONEY_TYPE.include?(value.to_sym)
+    MONEY_TYPE.money_list.include?(value.to_sym)
   end
 end
