2d495c11ee5ba7859b72be2626f3c393c9a9c33a
soom93 <soom93@gmail.com>
2019-06-02 14:43:38 +0900

    T1. 테스트 코드 수정 + Test1 완료

--- test.rb
+++ test.rb
@@ -1,19 +1,23 @@
 
 require 'test/unit' 
+require './vending_machine'
+require './user'
+require './money'
+require './drink'
 
 class VMTests < Test::Unit::TestCase
   def test_vm_overview
-    vm = VM.new()
-    user = User.new("10000")
+    vm = VendingMachine.new()
+    user = User.new(Money.new(10000))
     deposit = user.input_amount_to_vm(Money.new(100))
-    
+
+    vm.input_deposit(deposit)
+
     # 자판기가 음료수 리스트를 보여준다.
-    drink = user.select_drink("cola")
+    drink = user.select_drink(Drink.new("cola",1000))
 
-    vm.inserted(deposit)
-    vm.checked(drink)
+    user.pick_up_drink(vm.return_drink(drink))
 
-    user.pick_up_drink = vm.output()
-    vm.return_balance()
+    user.output_balance_from_vm(vm.return_balance())
   end
 end
