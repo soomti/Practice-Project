a441be9756539ee3163169715dba0ddfea588c28
soom93 <soom93@gmail.com>
2019-06-02 14:42:39 +0900

    T1. 테스트 코드 유저 수정

--- user.rb
+++ user.rb
@@ -1,14 +1,15 @@
 class User
   attr_accessor :amount
-  @drinks = []
 
   def initialize(amount)
-    @amount = amount
+    @amount = amount.value
+    @drinks = []
   end
 
   # 자판기에 동전을 넣는다
   def input_amount_to_vm(money)
-    @amount -= money
+    @amount -= money.value
+    money.value
   end
 
   # 자판기가 잔돈을 반환한다.
@@ -18,11 +19,12 @@ class User
 
   # 음료수를 선택한다.
   def select_drink(drink)
-    return 
+    drink
   end
 
   # 음료수를 받는다.
   def pick_up_drink(drink)
     @drinks.push(drink)
+    puts @drinks.first.name
   end
 end
