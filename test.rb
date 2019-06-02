952dc8cc0b196503158d398d1f4169f90f242b13
soom93 <soom93@gmail.com>
2019-06-02 13:47:29 +0900

    T1. 테스트 코드 유저 적용

--- test.rb
+++ test.rb
@@ -4,17 +4,16 @@ require 'test/unit'
 class VMTests < Test::Unit::TestCase
   def test_vm_overview
     vm = VM.new()
-    user = User.new()
-    deposit = user.input(Money.new(1000))
-
+    user = User.new("10000")
+    deposit = user.input_amount_to_vm(Money.new(100))
+    
     # 자판기가 음료수 리스트를 보여준다.
-
-    drink = user.pick()
+    drink = user.select_drink("cola")
 
     vm.inserted(deposit)
     vm.checked(drink)
 
-    vm.output()
+    user.pick_up_drink = vm.output()
     vm.return_balance()
   end
 end
