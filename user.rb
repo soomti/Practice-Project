4b25114401dda6efeea17606055b048b12be5185
soom93 <soom93@gmail.com>
2019-06-08 00:31:08 +0900

    T3. 유저 index 선택 적용

--- user.rb
+++ user.rb
@@ -1,3 +1,4 @@
+require './drink_type'
 class User
   attr_accessor :amount
 
@@ -6,7 +7,7 @@ class User
     @drinks = []
   end
 
-  # 자판기에 동전을 넣는다
+  # 자판기에 돈을 투입한다.
   def input_amount_to_vm(money)
     @amount -= money.value
     money.value
@@ -18,13 +19,13 @@ class User
   end
 
   # 음료수를 선택한다.
-  def select_drink(drink)
-    drink
+  def select_drink(drink_number)
+    drink_index = drink_number-1 
+    drink_index #index.. 고민 잘한건지 모르겠음 사용자는 1 이지만, 이제부턴 기계에 들어가서 바꿨는데, 잘 모르겠당. 
   end
 
   # 음료수를 받는다.
   def pick_up_drink(drink)
     @drinks.push(drink)
-    puts @drinks.first.name
   end
 end
