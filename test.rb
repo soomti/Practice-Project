1e0b685bb3008815f3945a0c8ef66f3d81425328
soom93 <soom93@gmail.com>
2019-06-08 00:30:21 +0900

    T3. 테스트 코드 작성

--- test.rb
+++ test.rb
@@ -7,18 +7,15 @@ require './drink'
 
 class VMTests < Test::Unit::TestCase
   def test_vm_overview
-    # vm = VendingMachine.new()
-    # user = User.new(Money.new(5000))
-    # deposit = user.input_amount_to_vm(Money.new(100))
-
-    # vm.input_deposit(deposit)
-
-    # # 자판기가 음료수 리스트를 보여준다.
-    # drink = user.select_drink(Drink.new("cola",1000))
-
-    # user.pick_up_drink(vm.return_drink(drink))
-
-    # user.output_balance_from_vm(vm.return_balance())
+    puts "-----TEST1"
+    vm = VendingMachine.new()
+    user = User.new(Money.new(5000))
+    deposit = user.input_amount_to_vm(Money.new(100))
+    vm.input_deposit(deposit)
+    drink = user.select_drink(2)
+    user.pick_up_drink(vm.return_drink(drink))
+    user.output_balance_from_vm(vm.return_balance())
+    puts "TEST1-----"
   end
 
   def test_money_valid
@@ -30,7 +27,7 @@ class VMTests < Test::Unit::TestCase
     vm.input_deposit(deposit)
 
     # 자판기가 음료수 리스트를 보여준다.
-    drink = user.select_drink(Drink.new("cola",1000))
+    drink = user.select_drink(1)
 
     user.pick_up_drink(vm.return_drink(drink))
 
@@ -41,6 +38,15 @@ class VMTests < Test::Unit::TestCase
     money2 = Money.new(250)
     money3 = Money.new(500)
 
+    vm.balance_calculate()
+
+    
+
+    puts "---test3---"
+    deposit = user.input_amount_to_vm(Money.new(1000))
+    vm.input_deposit(deposit)
+    drink_number = user.select_drink(1)
+    user.pick_up_drink(vm.return_drink(drink_number))
     vm.balance_calculate()
   end
 end
