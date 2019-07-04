875792c3b241f43e58f1260c6ac4510f7ff2ac98
soom93 <soom93@gmail.com>
2019-07-04 14:41:47 +0900

    gb - 티켓 메뉴를 안내한다

--- tm.rb
+++ tm.rb
@@ -1,6 +1,14 @@
 class TicketMachine
+  def self.start
+    "1. 티켓예매 2. 예매확인 3. 예매취소 4. 예매출력"
+  end
   def self.print
     true
     # "티켓이 성공적으로 출력되었습니다."
+  end
+
+  def self.get_menu
+    menu = gets.chomp
+    menu
   end
 end
